ecbe8ebf211471bd0d302f37e3f074ce0a0c7972
xqqlove <1977579350@qq.com>
2019-06-01 21:44:38 +0800

    spring 自动装配

---
+++ src/com/qq30springAutowire/Car.java
@@ -0,0 +1,30 @@
+package com.qq30springAutowire;
+
+public class Car {
+    private String brand;
+    private double price;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/com/qq30springAutowire/Person.java
@@ -0,0 +1,40 @@
+package com.qq30springAutowire;
+
+public class Person {
+    private String name;
+    private Address address;
+    private Car car;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                ", car=" + car +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+}
