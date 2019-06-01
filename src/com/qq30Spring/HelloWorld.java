4c1b893a678fb6fa751f7303805b53c25c7546a3
xqqlove <1977579350@qq.com>
2019-06-01 17:34:46 +0800

    springHelloworld方法上传

--- src/com/qq30Spring/HelloWorld.java
+++ src/com/qq30Spring/HelloWorld.java
@@ -1,4 +1,17 @@
 package com.qq30Spring;
 
 public class HelloWorld {
+    private String name;
+
+    public void setName(String name) {
+        System.out.println("setName");
+        this.name = name;
+    }
+
+    public void hello() {
+        System.out.println("hello:" + name);
+    }
+    public HelloWorld(){
+        System.out.println("init constructor");
+    }
 }
