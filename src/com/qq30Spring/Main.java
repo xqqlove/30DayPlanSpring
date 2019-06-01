04a2ccda5672ae515c2ba70824f643b33f6337a8
xqqlove <1977579350@qq.com>
2019-06-01 17:34:32 +0800

    springHelloworld方法上传

---
+++ src/com/qq30Spring/Main.java
@@ -0,0 +1,22 @@
+package com.qq30Spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static  void main(String[] args){
+    //创建对象以及给属性赋值交给spring操作
+        /**
+        HelloWorld helloWorld= new HelloWorld();//创建对象
+        helloWorld.setName("qiangqiang");//属性赋值
+        **/
+
+        //1.创建spring的IOC容器对象
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+        //2.从IOC容器获取bean的实例
+        HelloWorld helloWorld=(HelloWorld) ctx.getBean("helloworld");
+        //调用hello方法
+        helloWorld.hello();
+
+    }
+}
